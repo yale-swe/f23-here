1453b153ee416e3fe9b2249205e85178c4736640
phucd5 <phucduongh@gmail.com>
2023-10-14 15:05:42 -0400

    finished auth

--- server/models/User.js
+++ server/models/User.js
@@ -5,33 +5,38 @@ import { MessageSchema } from "./Message.js";
 const { Schema } = mongoose;
 
 export const UserSchema = new Schema({
-    id: {
-        type: Schema.Types.UUID,
-        required: true,
-        unique: true
-    },
-    userName: {
-        type: String,
-        required: true,
-    },
-    firstName: String,
-    lastName: String,
-    messages: [MessageSchema],
-    friends: [UserSchema],
-    notifyFriends: Boolean,
-    email: {
-        type: String,
-        required: [true, "Please enter a valid email"]
-    },
-    password: {
-        type: String,
-        required: [true, "Please enter a valid password"]
-    },
-    avatar: {
-        type: String,
-        default: "no-photo.jpg"
-    },
+	id: {
+		type: Schema.Types.UUID,
+		required: true,
+		unique: true,
+	},
+	userName: {
+		type: String,
+		required: true,
+	},
+	firstName: {
+		type: String,
+		required: true,
+	},
+	lastName: {
+		type: String,
+		required: true,
+	},
+	messages: [MessageSchema],
+	friends: [UserSchema],
+	notifyFriends: Boolean,
+	email: {
+		type: String,
+		required: [true, "Please enter a valid email"],
+	},
+	password: {
+		type: String,
+		required: [true, "Please enter a valid password"],
+	},
+	avatar: {
+		type: String,
+		default: "no-photo.jpg",
+	},
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
-
---
+++ server/routes/auth.js
@@ -0,0 +1,51 @@
+import User from "../models/User.js";
+import { handleServerError, handleSuccess } from "../utils/handlers.js";
+
+export const register = async (req, res) => {
+	try {
+		const { userName, password, email, firstName, lastName } = req.body;
+
+		const salt = await bcrypt.genSalt(10);
+		const passwordHash = await bcrypt.hash(password, salt);
+
+		const newUser = new User({
+			userName,
+			firstName,
+			lastName,
+			email,
+			password: passwordHash,
+		});
+
+		const savedUser = await newUser.save();
+		handleSuccess(res, savedUser);
+	} catch (err) {
+		handleServerError(res, err);
+	}
+};
+
+export const login = async (req, res) => {
+	try {
+		const { inputLogin, password } = req.body;
+
+		const user = await User.findOne({
+			$or: [{ email: inputLogin }, { username: inputLogin }],
+		});
+
+		if (!user) {
+			return handleNotFound(res);
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			return handleBadRequest(
+				res,
+				"Incorrect login information. Please try again."
+			);
+		}
+
+		delete user.password;
+		handleSuccess(res, user);
+	} catch (err) {
+		handleServerError(res, err);
+	}
+};
