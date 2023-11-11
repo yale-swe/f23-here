60a8c349cb15dce71b0e3e79efaba6ba018c6776
Xinran Li <xinranli5982@gmail.com>
2023-11-11 15:52:59 -0500

    adding error message pop-up in LoginView

--- app/newHere1/newHere/LoginView.swift
+++ app/newHere1/newHere/LoginView.swift
@@ -16,6 +16,9 @@ struct LoginView: View {
     @State private var isRegistered = false
     @Binding var isAuthenticated: Bool
     
+    @State private var showingAlert = false
+    @State private var alertMessage = ""
+    
     var body: some View {
         
         NavigationView {
@@ -29,7 +32,7 @@ struct LoginView: View {
                     .background(Color(.systemGray6))
                     .cornerRadius(5.0)
                     .padding(.bottom, 20)
-
+                
                 SecureField("Password", text: $password)
                     .padding()
                     .background(Color(.systemGray6))
@@ -46,7 +49,7 @@ struct LoginView: View {
                         .cornerRadius(5.0)
                 }
                 .padding(.horizontal)
-
+                
                 NavigationLink(destination: RegistrationView(isRegistered: $isRegistered)) {
                     Text("Don't have an account? Signup")
                 }
@@ -55,61 +58,96 @@ struct LoginView: View {
                 Spacer()
             }
             .padding()
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Login Status"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
         }
     }
-    
-    func LogInUser(){
-        print("Login user called")
-        let requestBody: [String: Any] = [
-            "inputLogin": username,
-            "password": password]
         
-        guard let jsonData = try? JSONSerialization.data(withJSONObject: requestBody)
-        else{
-            return
-        }
-        
-        guard let url = URL(string: loginUrlString) else {
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = jsonData
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue(logApiKey, forHTTPHeaderField: "x-api-key")
-
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error{
-                print("error:\(error)")
-            }
-            guard let httpResponse = response as? HTTPURLResponse else {
-                print("Invalid HTTP Response")
+        func LogInUser(){
+            if username.isEmpty || password.isEmpty {
+                self.alertMessage = "Please enter both username and password."
+                self.showingAlert = true
                 return
             }
-            let statusCode = httpResponse.statusCode
             
-            if statusCode == 200 {
-                if let data = data {
-                    if let responseString = String(data: data, encoding: .utf8) {
-                        print("Login Response: \(responseString)")
+            print("Login user called")
+            let requestBody: [String: Any] = [
+                "inputLogin": username,
+                "password": password]
+            
+            guard let jsonData = try? JSONSerialization.data(withJSONObject: requestBody)
+            else{
+                return
+            }
+            
+            guard let url = URL(string: loginUrlString) else {
+                return
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = jsonData
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue(logApiKey, forHTTPHeaderField: "x-api-key")
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                DispatchQueue.main.async {
+                    if let error = error{
+                        self.alertMessage = "Login failed: \(error.localizedDescription)"
+                        self.showingAlert = true
+                    }else if let httpResponse = response as? HTTPURLResponse{
+                        let statusCode = httpResponse.statusCode
+                        if statusCode == 200 {
+                            if let data = data {
+                                if let responseString = String(data: data, encoding: .utf8) {
+                                    print("Login Response: \(responseString)")
+                                }
+                                self.isAuthenticated = true;
+                            }
+                        }else if statusCode == 404 {
+                            self.alertMessage = "User not found. Please check your credentials."
+                            self.showingAlert = true
+                        }else{
+                            self.alertMessage = "Login failed: Server returned status code \(httpResponse.statusCode)"
+                            self.showingAlert = true
+                        }
+                    }else{
+                        // General error
+                        self.alertMessage = "Login failed: Unexpected error occurred"
+                        self.showingAlert = true
                     }
-                    self.isAuthenticated = true;
                 }
-            }
-            // else got error from server
-            else {
-                if let response = response {
-                    print("response:\(response)")
-                }
-            }
+                
+                //            guard let httpResponse = response as? HTTPURLResponse else {
+                //                self.alertMessage = "Login failed: Server returned status code \(httpResponse.statusCode)"
+                //                self.showingAlert = true
+                ////                print("Invalid HTTP Response")
+                //                return
+                //            }
+                //
+                //            let statusCode = httpResponse.statusCode
+                //
+                //            if statusCode == 200 {
+                //                if let data = data {
+                //                    if let responseString = String(data: data, encoding: .utf8) {
+                //                        print("Login Response: \(responseString)")
+                //                    }
+                //                    self.isAuthenticated = true;
+                //                }
+                //            }
+                //            // else got error from server
+                //            else {
+                //                if let response = response {
+                //                    print("response:\(response)")
+                //                }
+                //            }
+                
+            }.resume()
             
-        }.resume()
+        }
         
-    }
-
 }
-
 //// Preview Provider
 //struct LoginView_Previews: PreviewProvider {
 //    static var previews: some View {
