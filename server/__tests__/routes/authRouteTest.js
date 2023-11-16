50c2709a6fa0cabaa98c1ddfd9b9531d2c3ca135
phucd5 <phucduongh@gmail.com>
2023-11-16 01:09:22 -0500

    finished backend test

---
+++ server/__tests__/routes/authRouteTest.js
@@ -0,0 +1,35 @@
+import { jest } from "@jest/globals";
+import { register, login } from "../../controllers/auth";
+import express from "express";
+import router from "../../routes/auth";
+
+const app = express();
+app.use(express.json());
+app.use("/auth", router);
+
+describe("Auth Router", () => {
+	test("login register return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		register(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+	test("login should return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		login(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+});
---
+++ server/__tests__/routes/messageRouteTest.js
@@ -0,0 +1,77 @@
+import { jest } from "@jest/globals";
+import {
+	postMessage,
+	deleteMessage,
+	incrementLikes,
+	changeVisibility,
+	getAllMessages,
+} from "../../controllers/message.js";
+import express from "express";
+import router from "../../routes/message";
+
+const app = express();
+app.use(express.json());
+app.use("/message", router);
+
+describe("Message Router", () => {
+	test("postMessage register return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		postMessage(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+	test("deleteMessage should return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		deleteMessage(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+	test("incrementLikes should return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		incrementLikes(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+	test("changeVisibility should return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		changeVisibility(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+	test("getAllMessages should return 505 error", () => {
+		const req = null;
+		const res = {
+			status: jest.fn().mockReturnThis(),
+			send: jest.fn(),
+			json: jest.fn(),
+		};
+
+		changeVisibility(req, res);
+		expect(res.status).toHaveBeenCalledWith(500);
+		expect(res.json).toHaveBeenCalledWith(expect.anything());
+	});
+});
